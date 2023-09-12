865ccf86399e6f4182d09f06dda6680e28179cba
Brian West <brian@cubistcastle.com>
2023-09-12 13:44:19 -0500

    add original style files

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,14 @@
 import { defineConfig } from 'astro/config';
+import sassGlobImports from "vite-plugin-sass-glob-import";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
-export default defineConfig({});
+
+
+export default defineConfig({
+  integrations: [tailwind()],
+  vite: {
+    plugins: [sassGlobImports()],
+  },
+});
